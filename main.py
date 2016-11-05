6c39b5641fc01d1e90a9581c25338ec086c55d1b
Nathanael Farley <nasfarley88@gmail.com>
2016-11-05 09:47:21 +0000

    Added auto discovery of plugins in plugin folder.

--- main.py
+++ main.py
@@ -1,9 +1,23 @@
 #!/usr/bin/env python
+import os
+import sys
+import logging
+import importlib
+
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.DEBUG)
 
 from skybeard.plugins import Plugin
 
-# Should magically add it to the bot
-from plugins.postcats import PostCats
+PATH = "plugins/"
+sys.path.insert(0, PATH)
+
+logger.debug("os.listdir({}) = {}".format(PATH, os.listdir(PATH)))
+for f in os.listdir(PATH):
+    fname, ext = os.path.splitext(f)
+    importlib.import_module(fname)
+sys.path.pop(0)
+
 
 def main():
     updater = Plugin.updater
