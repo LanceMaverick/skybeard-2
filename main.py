f12a841dd0dc3ac491f34f04efdcf968fc6790e7
Nathanael Farley <nasfarley88@gmail.com>
2016-11-05 09:22:11 +0000

    Basic plugin system implemented.

--- main.py
+++ main.py
@@ -2,6 +2,9 @@
 
 from skybeard.plugins import Plugin
 
+# Should magically add it to the bot
+from plugins.postcats import PostCats
+
 def main():
     updater = Plugin.updater
     updater.start_polling()
@@ -9,6 +12,3 @@ def main():
 
 if __name__ == '___main__':
     main()
-
-
-
