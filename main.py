99793fa429bb881cdc560882a80d2e1402ff3a4a
Nathanael Farley <nasfarley88@gmail.com>
2016-11-05 19:21:06 +0000

    Configured to use ini file.

--- main.py
+++ main.py
@@ -3,31 +3,49 @@ import os
 import sys
 import logging
 import importlib
-import yaml
+import configparser
 import argparse
 
+logging.basicConfig(level=logging.DEBUG)
 logger = logging.getLogger(__name__)
 logger.setLevel(logging.DEBUG)
 
 from skybeard.beards import Beard
 
+def is_module(filename):
+    fname, ext = os.path.splitext(filename)
+    if ext == ".py":
+        return True
+    elif os.path.exists(os.path.join(filename, "__init__.py")):
+        return True
+    else:
+        return False
+
+def all_possible_beards(paths):
+    for path in paths:
+        for f in os.listdir(path):
+            if is_module(os.path.join(path, f)):
+                yield os.path.basename(f)
+
 def main(config):
     beard_path = "beards/"
     sys.path.insert(0, beard_path)
 
-    logger.debug("os.listdir({}) = {}".format(beard_path,
-                                              os.listdir(beard_path)))
-    if config["beards"] == "all":
-        for f in os.listdir(beard_path):
-            fname, ext = os.path.splitext(f)
-            importlib.import_module(fname)
-    elif isinstance(config["beards"], list):
-        for f in config["beards"]:
-            fname, ext = os.path.splitext(f)
-            importlib.import_module(fname)
-    else:
-        assert False, "Skybeard does not understand which beards to wear. Check your config file."
+    logger.debug("Available plugins: {}".format(
+        list(all_possible_beards([beard_path]))))
+    logger.debug("Config.main.beards: {}".format(config["main"]["beards"]))
+    requested_beards = config["main"]["beards"].split("\n")
 
+    # Remove empty firstline if needed
+    if requested_beards[0] == "":
+        requested_beards = requested_beards[1:]
+
+    if requested_beards == ["all"]:
+        for beard_name in all_possible_beards([beard_path]):
+            importlib.import_module(beard_name)
+    else:
+        for beard_name in requested_beards:
+            importlib.import_module(beard_name)
 
     sys.path.pop(0)
 
@@ -39,7 +57,7 @@ if __name__ == '__main__':
 
     parser = argparse.ArgumentParser(description='Skybeard hails you!')
 
-    parser.add_argument('-c', '--config-file', default="config.yaml")
+    parser.add_argument('-c', '--config-file', default="config.ini")
     parser.add_argument('-k', '--key', default=os.environ.get('TG_BOT_TOKEN'))
 
     parsed = parser.parse_args()
@@ -48,7 +66,8 @@ if __name__ == '__main__':
     Beard.setup_beard(parsed.key)
 
     logger.debug("Getting config")
-    config = yaml.load(open("config.yaml"))
+    config = configparser.ConfigParser()
+    config.read(parsed.config_file)
     logger.debug("Config found to be: {}".format(config))
 
 
