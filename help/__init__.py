5e9ca621f3ee77e2c611feae48ab0d1909f86009
Nathanael Farley <nasfarley88@gmail.com>
2016-11-18 16:28:17 +0000

    Automatic help searching of all beards implemented.

--- help/__init__.py
+++ help/__init__.py
@@ -1,17 +1,83 @@
-from telegram.ext import CommandHandler
-from skybeard.beards import Beard
+import re
+
+import telepot
+
+from skybeard.beards import BeardMixin
+
+import config
+
+def embolden(string):
+    return "<b>"+string+"</b>"
+
+def italisize(string):
+    return "<i>"+string+"</i>"
+
+async def fetch_user_help():
+    """A little bit of magic to fetch all the __userhelp__'s."""
+    retdict = dict()
+    for beard in BeardMixin.beards:
+        try:
+            retdict[beard.__class__.__name__] = beard.__userhelp__
+        except AttributeError:
+            retdict[beard.__class__.__name__] = None
+
+    return retdict
+
+async def format_user_help(userhelps):
+    """Takes a dict of user help messages and formats them."""
+
+    retstr = italisize("List of beard documentation:\n\n")
+    for name, userhelp in sorted(userhelps.items(), key=lambda x: x[0]):
+        if userhelp:
+            retstr += "{name}:\n{userhelp}\n\n".format(name=embolden(name), userhelp=userhelp)
+        else:
+            retstr += "{name}:\nNo documentation found.\n\n".format(name=embolden(name))
+
+    retstr += italisize("End of beard documentation.")
+
+    return retstr
+
+class Help(telepot.aio.helper.ChatHandler):
+
+    # __init__ is implicit
+
+    _timeout = 2
+
+    async def on_chat_message(self, msg):
+        try:
+            text = msg['text']
+        except KeyError:
+            return
+
+        if re.match("^/help(?!@.+)?$", text):
+            await self.send_help(msg)
+
+    @property
+    def __userhelp__(self):
+        return "\n".join([
+            "I'm the default help beard.",
+            "",
+            "/help - display the help for this bot."])
+
+    async def send_help(self, msg):
+        retstr = ""
+        try:
+            retstr += config.__userhelp__
+        except AttributeError:
+            retstr += ("My help message is unconfigured. To display "
+                        "something here, add a docstring to my config.py.")
+
+        userhelp = await fetch_user_help()
+        userhelp = await format_user_help(userhelp)
+        retstr += "\n\n{}".format(userhelp)
+        # TODO update to something more proper
+        await self.sender.sendMessage(retstr, parse_mode='html')
+
 
 def create_help(config):
-    class Help(Beard):
-        def initialise(self):
-            self.disp.add_handler(CommandHandler('help', self.send_help))
 
-        def send_help(self, bot, update):
-            if config.__doc__:
-                update.message.reply_text(config.__doc__)
-            else:
-                update.message.reply_text(
-                    ("My help message is unconfigured. To display "
-                    "something here, add a docstring to my config.py."))
+    class BeardedHelp(Help, BeardMixin):
+        pass
 
-    return Help
+    return BeardedHelp
+
