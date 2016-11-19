15c4beb5206dccbeb236cd524b771364470ffbc7
Nathanael Farley <nasfarley88@gmail.com>
2016-11-19 13:40:26 +0000

    Moved movies beard to telepot.

---
+++ beards/movies/__init__.py
@@ -0,0 +1,27 @@
+# from telegram.ext import CommandHandler
+import telepot
+import telepot.aio
+from urllib.request import urlopen
+from skybeard.beards import BeardAsyncChatHandlerMixin
+from . import movies
+
+class Movies(telepot.aio.helper.ChatHandler, BeardAsyncChatHandlerMixin):
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.register_command('movie', self.movie_search)
+
+    async def movie_search(self, msg):
+        movie_title = msg['text'].split('/movie',1)[1]
+        if not movie_title:
+            await self.sender.sendMessage('please specify a movie title')
+        else:
+            search = movies.search(movie_title)
+            if search["photourl"]:
+                try:
+                    await self.sender.sendPhoto(("poster.jpg", urlopen(search["photourl"])))
+                except telepot.exception.TelegramError:
+                    await self.sender.sendMessage(search["photourl"])
+            await self.sender.sendMessage(search["text"])
+
+
---
+++ beards/movies/movies.py
@@ -0,0 +1,44 @@
+import omdb
+
+def search(title):
+    #makes the imdb search url if match not found
+    def build_imdb_url(title):
+        title_list = [element.strip() for element in title.split(' ')]
+        url_elements = [
+                'http://www.imdb.com/find?ref_=nv_sr_fn&q=',
+                '+'.join(title_list),
+                '&s=all'
+                ]
+        url = ''.join(url_elements)
+        return url
+
+    #poll api
+    result = omdb.get(title=title)
+    if not result:
+        return {"text": ('I could not find what you were looking for. '
+                         'Try here: \n\n'+\
+                         build_imdb_url(title)),
+                "photourl": None}
+    else:
+        film = result.title
+        year = result.year
+        director = result.director
+        metascore = result.metascore
+        imdbscore = result.imdb_rating
+        plot = result.plot
+        poster = result.poster
+        imdb = 'http://www.imdb.com/title/'+result.imdb_id
+
+        #exception handling for movies with no poster
+        # message.reply_photo(photo=poster)
+        reply = (
+                'Title: '+film+'\n'
+                'Year: '+year+'\n'
+                'Director: '+director+'\n'
+                'Metascore: '+metascore+'\n'
+                'IMDb rating: '+imdbscore+'\n'
+                'Plot:\n'+plot
+                )
+
+        return {"text": reply+"\n\n"+imdb,
+                "photourl": poster}
