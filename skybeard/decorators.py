cbbe28c57c31ae61fdae51c69d65b52ec6dd5616
Nathanael Farley <nasfarley88@gmail.com>
2017-01-13 14:57:28 +0000

    Improved onerror decorator.

--- skybeard/decorators.py
+++ skybeard/decorators.py
@@ -1,19 +1,54 @@
-from functools import wraps
+from functools import wraps, partial
 import logging
 
 logger = logging.getLogger(__name__)
 
 
-def onerror(f):
-    @wraps(f)
+def onerror(f_or_text=None, **kwargs):
+    """A decorator for sending a message to the user on an exception.
+
+    If no arguments are used (i.e. the function is passed directly to the
+    decorator), beard.__onerror__(exception) is called if the decorated
+    function excepts.
+
+    If a string is passed as the first argument, then the decorated function
+    sends this message instead of calling the beard.__onerror__ function.
+    kwargs are passed to beard.sender.sendMessage.
+
+    If only kwargs are passed, then the decorated function attempts
+    beard.sender.sendMessage(**kwargs).
+
+    """
+    if isinstance(f_or_text, str):
+        return partial(onerror, text=f_or_text, **kwargs)
+    elif f_or_text is None:
+        return partial(onerror, **kwargs)
+
+    @wraps(f_or_text)
     async def g(beard, *fargs, **fkwargs):
         try:
-            return await f(beard, *fargs, **fkwargs)
+            return await f_or_text(beard, *fargs, **fkwargs)
         except Exception as e:
-            await beard.__onerror__(e)
+            if kwargs:
+                await beard.sender.sendMessage(**kwargs)
+            else:
+                await beard.__onerror__(e)
             raise e
 
     return g
+
+
+def onerror_with_message(message, parse_args=None):
+    def _onerror_with_message(f):
+        @wraps(f)
+        async def g(beard, *fargs, **fkwargs):
+            try:
+                return await f(beard, *fargs, **fkwargs)
+            except Exception as e:
+                await beard.sender.sendMessage(message, parse_args=parse_args)
+                raise e
+
+        return g
 
 
 def debugonly(f):
