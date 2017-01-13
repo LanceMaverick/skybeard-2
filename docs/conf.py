0c3509d2d3d2dff7f0803391a5cca72ff95a1e1b
LanceMaverick <pnoel6@gmail.com>
2017-01-13 22:52:05 +0000

    mock module fix

--- docs/conf.py
+++ docs/conf.py
@@ -17,6 +17,11 @@
 # add these directories to sys.path here. If the directory is relative to the
 # documentation root, use os.path.abspath to make it absolute, like shown here.
 #
+#trick to fix autodocs
+import mock
+MOCK_MODULES = ['functools', 'asyncio', 're', 'json', 'traceback', 'telepot.aio', 'telepot', 'config']
+for mod_name in MOCK_MODULES:
+    sys.modules[mod_name] = mock.Mock()
 import os
 import sys
 sys.path.insert(0, os.path.abspath('.'))
