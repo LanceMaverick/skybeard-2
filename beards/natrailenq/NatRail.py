32ede83c51f30af33b54a9f1b712de18a99e1e6f
artemis-beta <KXZ186@student.bham.ac.uk>
2017-01-13 11:37:01 +0000

    Can now specify a destination

--- beards/natrailenq/NatRail.py
+++ beards/natrailenq/NatRail.py
@@ -12,9 +12,15 @@ class RailScraper:
         self.codes = json.load(open(station_codes_doc, 'r'))     
         self.info = {}
 
-      def getDepartures(self, station):
+      def planJourney(self, station, To, time='now'):
+        
+
+      def getDepartures(self, station, To=''):
+        if To != '':
+          To = self.codes[To]
+          To = '/{}/To'.format(To)
         stc = self.codes[station.replace('/departures ','')]
-        webpage = requests.get(self.coreurl+stc)
+        webpage = requests.get(self.coreurl+stc+To)
         i=1
         result = re.findall(r'(?:\s{29})([\w\s\&\.\)\(\']+)&\w+;(.*?)<\/td>', webpage.content.decode('utf-8'))	
         times = re.findall(r'(\d\d:\d\d|Cancelled|On time)', webpage.content.decode('utf-8'))
@@ -30,17 +36,18 @@ class RailScraper:
            self.info[i]['due'] = due[i-1]
            self.info[i]['expected'] = expt[i-1]
            i+=1
-      def makeDeptString(self, station):
-           self.getDepartures(station)
+      def makeDeptString(self, station, via=''):
+           self.getDepartures(station, via)
            out_str = ''
            if bool(self.info):
             print("Passed")
             for key in self.info:
-             out_str +='{} {}'.format(self.info[key]['due'],self.info[key]['destination'])
+             out_str +='*{}*\t{}'.format(self.info[key]['due'], self.info[key]['destination'])
              if self.info[key]['via'] != '':
-                out_str += ' via {}'.format(self.info[key]['via'])
-             out_str += ' status: {}\n'.format(self.info[key]['expected'])
+                out_str += '\tvia {}'.format(self.info[key]['via'])
+             out_str += '\t_{}_\n'.format(self.info[key]['expected'])
              print(out_str)
+            
            else:
               out_str = "No Services From This Station."
            return out_str
--- beards/natrailenq/__init__.py
+++ beards/natrailenq/__init__.py
@@ -4,6 +4,7 @@ import telepot
 import telepot.aio
 from skybeard.beards import BeardChatHandler, regex_predicate
 from . import NatRail
+import re
 
 class NationalRailDepartures(BeardChatHandler):
     __userhelp__ = """
@@ -11,11 +12,20 @@ class NationalRailDepartures(BeardChatHandler):
      """
 
     __commands__ = [
-        ("departures", "checkTimes", "Calculates route from [origin] to [destination]"),
+        ("departures", "checkTimes", "Displays departures from stated station.\n optional argument ' to [destination]' can be added to refine search."),
     ]
 
 
     async def checkTimes(self, msg):
         natRail = NatRail.RailScraper('http://ojp.nationalrail.co.uk/service/ldbboard/dep/', 'beards/natrailenq/station_codes.json')
-        out = natRail.makeDeptString(msg['text'])
-        await self.sender.sendMessage(out)
+        out = msg['text'].replace('/departures ','')
+        res1 = re.findall(r'(.+)\s(?:To|TO|to)\s(.+)', out)
+        if len(res1) == 0:
+          res1 = re.findall(r'(.+)', out)
+          From  = res1[0] 
+          To = ''
+        else:
+          From, To = res1[0]
+        print(res1[0])
+        out = natRail.makeDeptString(From,To)
+        await self.sender.sendMessage(out, parse_mode="Markdown")
