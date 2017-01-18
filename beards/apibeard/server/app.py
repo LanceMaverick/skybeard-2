8c894057500d5819862fc36b916d4efdab42f1d3
Nathanael Farley <nasfarley88@gmail.com>
2017-01-18 00:42:11 +0000

    Added CORS to apibeard app.

--- beards/apibeard/server/app.py
+++ beards/apibeard/server/app.py
@@ -5,6 +5,11 @@ from sanic import Sanic, Blueprint
 from sanic.response import json, text
 from sanic.exceptions import NotFound
 
+try:
+    from sanic_cors import CORS
+except ImportError:
+    assert False, "Missing dependency (sanic_cors). To install: pip install https://github.com/ashleysommer/sanic-cors/"
+
 from skybeard.beards import Beard
 
 from . import telegram as tg
@@ -15,6 +20,9 @@ logger = logging.getLogger(__name__)
 
 
 app = Sanic(__name__)
+
+# CORS: https://en.wikipedia.org/wiki/Cross-origin_resource_sharing
+CORS(app)
 key_blueprint = Blueprint('key', url_prefix='/key[A-z]+')
 
 
@@ -23,8 +31,8 @@ async def hello_world(request):
     return text(
         ("Hello World! Your API beard is working! "
          "Running Sanic version: {}. Beards running: {}.").format(
-        sanic.__version__,
-        Beard.beards
+             sanic.__version__,
+             Beard.beards
     ))
 
 
