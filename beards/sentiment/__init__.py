21bcbaa4e0fb16632ff3191c0d15f040d98a9eac
LanceMaverick <pnoel6@gmail.com>
2017-01-31 00:19:18 +0000

    removed redundant /score check in sentiment beard

--- beards/sentiment/__init__.py
+++ beards/sentiment/__init__.py
@@ -24,29 +24,25 @@ class SentBeard(BeardChatHandler):
 
     
     async def log_message_score(self, msg):
-        #don't bother saving /score messages as they are likely not
-        #representitive of chat
-        if not msg['text'].startswith('/score'):
-            score = sent.analyze(msg)
-            #responds message if a user sends a really negative message.
-            #Set the cutoff for this in in config.py
-            if score['compound']< config.cut:
+        score = sent.analyze(msg)
+        #responds message if a user sends a really negative message.
+        #Set the cutoff for this in in config.py
+        if score['compound']< config.cut:
 
-                #Uncomment to send a gif in the folder saved as "upset.gif".
-                #For example: http://knowyourmeme.com/photos/481115-flying-lawnmower
-                curr_path = os.path.dirname(__file__)
-                try:
-                    await self.sender.sendDocument((
-                            'upset.gif', 
-                            open(
-                                os.path.join(curr_path, 'upset.gif'),'rb' )))
-                except FileNotFoundError:
-                    await self.sender.sendMessage(
-                            '{}, there is no need to be upset'.format(
-                                msg['from']['first_name']))
-                    
-            
-            await sent.save(msg, score)
+            #Uncomment to send a gif in the folder saved as "upset.gif".
+            #For example: http://knowyourmeme.com/photos/481115-flying-lawnmower
+            curr_path = os.path.dirname(__file__)
+            try:
+                await self.sender.sendDocument((
+                        'upset.gif', 
+                        open(
+                            os.path.join(curr_path, 'upset.gif'),'rb' )))
+            except FileNotFoundError:
+                await self.sender.sendMessage(
+                        '{}, there is no need to be upset'.format(
+                            msg['from']['first_name']))
+        
+        await sent.save(msg, score)
 
     async def report(self, msg):
         plot1, plot2, plot3, plot4 = sent.get_results(msg, neut= False)
