ab172cbdecbec5a3ffcc1ef1f7f81e883e15e490
Growleythebear <wgrowley@gmail.com>
2017-02-04 19:44:14 +0000

    Rewrite of corgibase to dynamically pull off reddit

--- beards/corgibase/__init__.py
+++ beards/corgibase/__init__.py
@@ -5,28 +5,40 @@ import logging
 from urllib.request import urlopen
 import telepot
 import telepot.aio
+import praw
 from skybeard.beards import BeardChatHandler
 from skybeard.predicates import regex_predicate
+from . import config
 
-class CorgiBase(BeardChatHandler):
+class corgibase(BeardChatHandler):
     __userhelp__ = """
     Say give me corgis or show me corgis to see some corgis!"""
 
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
         self.register_command(
-            regex_predicate('(give|show) me corgis'), self.send_corgi)
+        regex_predicate('(give|show) me corgis'), self.send_corgi)
 
     async def send_corgi(self, msg):
-        corgi_photos= [
-                'http://i.imgur.com/vE7TLA2.jpg',
-		'https://i.imgur.com/v0ZzD8O.jpg',
-		'http://i.imgur.com/XBNueYD.jpg',
-		'http://i.imgur.com/ATa8cDd.jpg',
-                ]
+        reddit = praw.Reddit(client_id = config.client_id, 
+                             client_secret = config.client_secret,
+                             username = config.username,
+                             password = config.password,
+                            )
+        subreddit = reddit.subreddit('corgis')
+        hot_posts = subreddit.hot(limit=10)
+        url_list = [post.url for post in hot_posts]
+
+
+#        corgi_photos= [
+ #               'http://i.imgur.com/vE7TLA2.jpg',
+#		'https://i.imgur.com/v0ZzD8O.jpg',
+#		'http://i.imgur.com/XBNueYD.jpg',
+#		'http://i.imgur.com/ATa8cDd.jpg',
+ #               ]
 
         try:
-            choice = random.choice(corgi_photos)
+            choice = random.choice(url_list)
             await self.sender.sendPhoto((choice.split("/")[-1], urlopen(choice)))
         except Exception as e:
             logging.error(e)
