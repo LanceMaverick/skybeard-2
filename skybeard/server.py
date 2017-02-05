4d614c1dcb8cb7a198a7f55fb01aed0cd099a3c8
LanceMaverick <pnoel6@gmail.com>
2017-02-05 02:01:48 +0000

    added add_post decorator and available commands endpoint

--- skybeard/server.py
+++ skybeard/server.py
@@ -1,5 +1,6 @@
 from aiohttp import web
 from functools import wraps, partial
+from skybeard.beards import Beard, SlashCommand
 
 app = web.Application()
 
@@ -14,9 +15,34 @@ def async_get(endpoint_or_fn, endpoint=None):
 
     return ret_func
 
+def async_post(endpoint_or_fn, endpoint=None):
+    if isinstance(endpoint_or_fn, str):
+        return partial(async_post, endpoint=endpoint_or_fn)
+
+    ret_func = wraps(endpoint_or_fn)(endpoint_or_fn)
+
+    app.router.add_post(endpoint, ret_func)
+
+    return ret_func
 
 @async_get('/')
 async def hello(request):
     return web.json_response({"text": "Hello, world"})
 
-# app.router.add_get('/', hello)
+@async_get('/loadedBeards')
+async def loaded_beards(request):
+    return web.json_response([str(x) for x in Beard.beards])
+
+@async_get('/availableCommands')
+async def available_commands(request):
+    d = {}
+    for beard in Beard.beards:
+        cmds = []
+        for cmd in beard.__commands__:
+            if isinstance(cmd, SlashCommand):
+                cmds.append(dict(command = cmd.cmd, hint = cmd.hlp))
+        if cmds:
+            d[beard.__name__] = cmds
+
+    return web.json_response(d)
+    
