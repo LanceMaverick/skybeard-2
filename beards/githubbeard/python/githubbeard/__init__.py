ec3c4152107249400c86ff0d1e7fd52d1b3d45ea
Nathanael Farley <nasfarley88@gmail.com>
2017-02-17 17:25:00 +0000

    Improved error checking of currentuserrepos of githubbeard.

--- beards/githubbeard/python/githubbeard/__init__.py
+++ beards/githubbeard/python/githubbeard/__init__.py
@@ -1,28 +1,25 @@
 from skybeard.beards import BeardChatHandler
-from skybeard.predicates import Filters
 from skybeard.utils import get_beard_config, get_args
 from skybeard.decorators import onerror
 
-from github import Github
+from github import Github, GithubException
+from github.GithubException import UnknownObjectException
 import maya
 
 CONFIG = get_beard_config()
+
 
 class GithubBeard(BeardChatHandler):
 
     __userhelp__ = "Github. In a beard."
 
-    # Commands takes tuples like arguments:
-    # 1. Condition: Predicate function/string telegram command e.g. "dog"
-    #    becomes # /dog
-    # 2. Callback: Coroutine or name of member coroutine as a string to call
-    #    when condition is met
-    # 3. Help: Help text
     __commands__ = [
-        # condition,   callback coro,             help text
-        ("currentusersrepos",     'get_current_user_repos',      'Echos everything said by anyone.'),
-        ("getrepo", "get_repo", "Gets information about given repo specifed in 1st arg."),
-        ("getpr", "get_pending_pulls", "Gets pending pull requests from specified repo (1st arg)"),
+        ("currentusersrepos", 'get_current_user_repos',
+         'Lists clickable links to repos of specifed user.'),
+        ("getrepo", "get_repo",
+         "Gets information about given repo specifed in 1st arg."),
+        ("getpr", "get_pending_pulls",
+         "Gets pending pull requests from specified repo (1st arg)"),
     ]
 
     def __init__(self, *args, **kwargs):
@@ -69,9 +66,14 @@ class GithubBeard(BeardChatHandler):
     async def get_current_user_repos(self, msg):
         args = get_args(msg)
         try:
-            user = self.github.get_user(args[0])
-        except IndexError:
-            user = self.github.get_user()
+            try:
+                user = self.github.get_user(args[0])
+            except IndexError:
+                user = self.github.get_user()
+        except UnknownObjectException:
+            await self.sender.sendMessage("User not found.")
+            return
+
         name = user.name or user.login
         await self.sender.sendMessage("Github repos for {}:".format(name))
         await self.sender.sendChatAction(action="typing")
