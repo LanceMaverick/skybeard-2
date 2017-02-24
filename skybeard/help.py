042c67a4df8fc8a61a23bf0ca2b5d98d4db38681
Nathanael Farley <nasfarley88@gmail.com>
2017-02-24 10:22:01 +0000

    Added /helppaged command.

--- skybeard/help.py
+++ skybeard/help.py
@@ -2,6 +2,7 @@ import telepot
 
 # from skybeard.beards import BeardAsyncChatHandlerMixin
 from skybeard.beards import BeardChatHandler, Beard, SlashCommand
+from skybeard.mixins import PaginatorMixin
 from skybeard.utils import embolden, italisize
 
 import config
@@ -27,19 +28,42 @@ async def fetch_user_help():
     return retdict
 
 
-async def format_user_help(userhelps):
+async def format_user_help(name, userhelp):
+    """Takes a dict of user help messages and formats them."""
+
+    retstr = ""
+    if userhelp:
+        retstr += "{name}:\n{userhelp}\n\n".format(name=embolden(name), userhelp=userhelp)
+    else:
+        retstr += "{name}:\nNo documentation found.\n\n".format(name=embolden(name))
+
+    return retstr
+
+
+async def format_user_helps(userhelps):
     """Takes a dict of user help messages and formats them."""
 
     retstr = italisize("List of beard documentation:\n\n")
     for name, userhelp in sorted(userhelps.items(), key=lambda x: x[0]):
-        if userhelp:
-            retstr += "{name}:\n{userhelp}\n\n".format(name=embolden(name), userhelp=userhelp)
-        else:
-            retstr += "{name}:\nNo documentation found.\n\n".format(name=embolden(name))
+        retstr += await format_user_help(name, userhelp)
 
     retstr += italisize("End of beard documentation.")
 
     return retstr
+
+
+async def format_user_helps_paginated(userhelps):
+    """Takes a dict of user help messages and formats them."""
+
+    retlist = []
+    for name, userhelp in sorted(userhelps.items(), key=lambda x: x[0]):
+        retstr = italisize("Beard documentation:\n\n")
+        retstr += await format_user_help(name, userhelp)
+        # retstr += italisize("End of beard documentation.")
+
+        retlist.append(retstr)
+
+    return retlist
 
 
 def get_all_cmd_helps():
@@ -65,10 +89,29 @@ class Help(telepot.aio.helper.ChatHandler):
             retstr += ("My help message is unconfigured. To display "
                        "something here, add a docstring to my config.py.")
 
-        userhelp = await fetch_user_help()
-        userhelp = await format_user_help(userhelp)
-        retstr += "\n\n{}".format(userhelp)
+        userhelps = await fetch_user_help()
+        userhelps = await format_user_helps(userhelps)
+        retstr += "\n\n{}".format(userhelps)
         await self.sender.sendMessage(retstr, parse_mode='html')
+
+    async def send_paginated_help(self, msg):
+        """sends the user a combined help message for all plug-ins"""
+        retstr = ""
+        try:
+            retstr += config.__userhelp__
+        except AttributeError:
+            retstr += ("My help message is unconfigured. To display "
+                       "something here, add a docstring to my config.py.")
+
+        userhelps = await fetch_user_help()
+        userhelps = await format_user_helps_paginated(userhelps)
+
+        async def identity(x):
+            return x
+
+        await self.send_paginated_message(userhelps, identity)
+        # retstr += "\n\n{}".format(userhelps)
+        # await self.sender.sendMessage(retstr, parse_mode='html')
 
     async def cmd_helps(self, msg):
         """sends the user a formatted list of commands for easy registering with
@@ -81,11 +124,12 @@ class Help(telepot.aio.helper.ChatHandler):
 
 def create_help(config):
 
-    class BeardedHelp(Help, BeardChatHandler):
+    class BeardedHelp(Help, PaginatorMixin, BeardChatHandler):
         """Beard for interfacing help functionality with telegram"""
         _timeout = 2
         __commands__ = [
             ('help', 'send_help', "Shows verbose help message."),
+            ('helppaged', 'send_paginated_help', "Shows verbose help message, paginated."),
             ('cmdhelps', 'cmd_helps', "Lists all commands available."),
         ]
 
