3bf4992b5c2df4fdefad824ba297196a5e0aca72
Nathanael Farley <nasfarley88@gmail.com>
2017-02-24 21:02:37 +0000

    Neatened code.

--- dashboard/app.js
+++ dashboard/app.js
@@ -1,79 +1,56 @@
 (function(){
-    var app = angular.module( "APIBeardDemo", ['ng'] );
+  var app = angular.module( "APIBeardDemo", ['ng'] );
+  app.service(
+    "APIBeardService",
+    ['$http', function APIBeardFunction($http) {
+      scope = this;
 
-    // function APIBeardFunction($http) {
-    //     this.loadedBeards = $http.get("http://localhost:8000/loadedBeards");
-    // }
+      this.beards = [];
 
-    app.service("APIBeardService",
-                ['$http', function APIBeardFunction($http) {
-                    scope = this;
+      this.setBeards = function(beards) {
+        this.beards = beards;
+      };
 
-                    this.beards = [];
+      this.getBeards = function() {
+        return this.beards;
+      };
 
-                    this.setBeards = function(beards) {
-                        this.beards = beards;
-                    };
+      this.fetchBeards = function(){
+        $http.get("http://localhost:8000/loadedBeards").then( function(data) {
+          scope.setBeards(data.data);
+        });
+      };
+    }
+    ]);
 
-                    this.getBeards = function() {
-                        return this.beards;
-                    };
+  app.controller(
+    'PanelController',
+    ["APIBeardService", function(APIBeardService) {
+      this.tab = 2;
 
-                    this.fetchBeards = function(){
-                        $http.get("http://localhost:8000/loadedBeards").then( function(data) {
-                            scope.setBeards(data.data);
-                        });
-                    };
-                }
-                ]);
-    // app.service("APIBeardService", ['$http', function() {}]);
-
-    // app.service('APIBeardService',
-    //             ['$http',
-    //              function($http) {
-
-    //              }]);
-
-    app.controller(
-        'PanelController',
-        ["APIBeardService", function(APIBeardService) {
-            this.tab = 2;
-
-            this.selectTab = function(setTab) {
-                this.tab = setTab;
-                // TODO make it not hard coded
-                if(this.tab === 1) {
-                    APIBeardService.fetchBeards();
-                };
-            };
-
-            this.isSelected = function(checkTab) {
-                return this.tab === checkTab;
-            };
-        }]);
-
-    app.controller(
-        'loadedBeardsController',
-        ['APIBeardService', function(APIBeardService) {
-            this.APIBeardService = APIBeardService;
-            // store = this;
-            // this.beards = [];
-
-            // this.loadBeards = function() {
-            //     // TODO do if else statement somewhere so that loading beards is
-            //     // text, and the beards are bullet points
-            //     this.beards = ['Loading beards...'];
-            //     $http.get("http://localhost:8000/loadedBeards").then(
-            //         function(data) {
-            //             store.beards = data.data;
-            //         });
-            // };
-        }]);
-
-    app.directive('loadedBeards', function(){
-        return {
-            restrict: "E",
-            templateUrl: "loaded-beards.html"
+      this.selectTab = function(setTab) {
+        this.tab = setTab;
+        // TODO make it not hard coded
+        if(this.tab === 1) {
+          APIBeardService.fetchBeards();
         };
-    });
+      };
+
+      this.isSelected = function(checkTab) {
+        return this.tab === checkTab;
+      };
+    }]);
+
+  app.controller(
+    'loadedBeardsController',
+    ['APIBeardService', function(APIBeardService) {
+      this.APIBeardService = APIBeardService;
+    }]);
+
+  app.directive('loadedBeards', function(){
+    return {
+      restrict: "E",
+      templateUrl: "loaded-beards.html"
+    };
+  });
 })();                           // End of closure
