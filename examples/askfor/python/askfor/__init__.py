5e5e6c023128b9f6be3fa79f881dc845d2350e23
Nathanael Farley <nasfarley88@gmail.com>
2017-04-28 16:20:38 +0100

    Added getargsorask decorator.

--- examples/askfor/python/askfor/__init__.py
+++ examples/askfor/python/askfor/__init__.py
@@ -1,6 +1,5 @@
 from skybeard.beards import BeardChatHandler
-from skybeard.decorators import onerror, askfor
-from skybeard.utils import get_args
+from skybeard.decorators import onerror, askfor, getargsorask
 
 
 class Askfor(BeardChatHandler):
@@ -9,7 +8,8 @@ class Askfor(BeardChatHandler):
 
     __commands__ = [
         # command, callback coro, help text
-        ("askforstuff", 'ask_for_stuff', 'Asks for two arguments and echos them back.')
+        ("askforstuff", 'ask_for_stuff', 'Asks for two arguments and echos them back.'),
+        ("optionalargsdemo", 'optional_args_demo', 'Asks for arguments if not provided (2 args.)'),
     ]
 
     # __init__ is implicit
@@ -18,5 +18,12 @@ class Askfor(BeardChatHandler):
     @askfor([('var_x', "What's your first variable?"),
              ('var_y', "What's your second variable?")])
     async def ask_for_stuff(self, msg, var_x, var_y):
+        await self.sender.sendMessage("1) {}\n2) {}".format(
+            var_x, var_y))
+
+    @onerror
+    @getargsorask([('var_x', "What's your first variable?"),
+                   ('var_y', "What's your second variable?")])
+    async def optional_args_demo(self, msg, var_x, var_y):
         await self.sender.sendMessage("1) {}\n2) {}".format(
             var_x, var_y))
--- skybeard/decorators/__init__.py
+++ skybeard/decorators/__init__.py
@@ -38,6 +38,31 @@ def _getargs(f, text="No args provided.", return_string=None):
     return g
 
 
+def getargsorask(vars_n_qs):
+    """TODO docs."""
+    return partial(_getargsorask,
+                   vars_n_qs=vars_n_qs)
+
+
+def _getargsorask(f, vars_n_qs):
+    @wraps(f)
+    async def g(beard, msg):
+        args = get_args(msg)
+        kwargs = dict()
+        for (i, (var, question)) in enumerate(OrderedDict(vars_n_qs).items()):
+            try:
+                kwargs[var] = args[i]
+            except IndexError:
+                await beard.sender.sendMessage(question)
+                # TODO something fancy if there's a timeout here
+                resp = await beard.listener.wait()
+                kwargs[var] = resp['text']
+
+        await f(beard, msg, **kwargs)
+
+    return g
+
+
 def askfor(vars_n_qs):
     """TODO docs."""
     return partial(_askfor,
