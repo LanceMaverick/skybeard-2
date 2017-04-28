c173a52c460f079ccc349dd237a5b0720ff4945f
Nathanael Farley <nasfarley88@gmail.com>
2017-04-28 16:29:45 +0100

    Added docs to askfor decorator.

--- skybeard/decorators/askfor.py
+++ skybeard/decorators/askfor.py
@@ -3,7 +3,25 @@ from functools import wraps, partial
 
 
 def askfor(vars_n_qs):
-    """TODO docs."""
+    """Ask for variables given to a function.
+
+    .. code:: python
+        @askfor([('var_x', "What's your first variable?"),
+                ('var_y', "What's your second variable?")])
+        async def ask_for_stuff(self, msg, var_x, var_y):
+            await self.sender.sendMessage("1) {}\n2) {}".format(
+                var_x, var_y))
+
+    Will result in:
+    > /askforstuff
+    < What's your first variable?
+    > 123
+    < What's your second variable?
+    > 456
+    < 1) 123
+      2) 456
+
+    """
     return partial(_askfor,
                    vars_n_qs=vars_n_qs)
 
