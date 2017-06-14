af442fece919e6452779413f3ecc56ca298e6d2d
LanceMaverick <pnoel6@gmail.com>
2017-06-14 19:26:05 +0200

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+
+config = {
+    'description': 'A plugin based telegram bot',
+    'author': 'LanceMaverick,',
+    'url': 'https://github.com/LanceMaverick/skybeard-2/',
+    'download_url': 'https://github.com/LanceMaverick/skybeard-2/releases/tag/2.1',
+    'version': '2.1',
+    'packages': ['skybeard'],
+    'name': 'skybeard',
+}
+
+setup(**config)
