e49182e6c91050ee18aca5a4d0a579d9c4922afb
Nathanael Farley <nasfarley88@gmail.com>
2017-06-14 14:02:40 +0100

    Added return_string arg to getargsorask.

--- skybeard/decorators/getargsorask.py
+++ skybeard/decorators/getargsorask.py
@@ -4,7 +4,7 @@ from functools import wraps
 from ..utils import get_args
 
 
-def getargsorask(vars_n_qs):
+def getargsorask(vars_n_qs, return_string=False):
     """A combination of getargs and askfor decorator.
 
     If the args are not provided during the function call, they will be asked
@@ -14,7 +14,13 @@ def getargsorask(vars_n_qs):
     def _getargsorask_wrapper(f):
         @wraps(f)
         async def g(beard, msg):
-            args = get_args(msg)
+            args = get_args(msg, return_string)
+
+            # If return_string is enabled, the 'args' will be a single string.
+            # Convert this into a single item argument list.
+            if return_string:
+                args = [args]
+
             kwargs = dict()
             for (i, (var, question)) in enumerate(OrderedDict(vars_n_qs).items()):
                 try:
